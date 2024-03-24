a4fdc565b0da93867014085ad64858ee2bdbe751
jasmitamukherjee <jasmita.mukherjee.btech2021@sitpune.edu.in>
2024-03-24 21:42:14 +0530

    added a color changing shape

--- App.js
+++ App.js
@@ -3,21 +3,29 @@ import { useState } from 'react';
 import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 export default function App() {
   const [randomBackground, setRandomBackground] = useState('#ffffff')
+  const [shapeColor, setShapeColor] = useState('#ffffff')
   const generateColor=()=>{
     const hexRange= '0123456789ABCDEF'
     let color= '#'
+    let scolor= '#'
 
     for(let i=0;i<6;i++){
     color += hexRange[Math.floor(Math.random() * 16)]
+    scolor+= hexRange[Math.round(Math.random() * 16)]
 
     }
     setRandomBackground(color)
+    setShapeColor(scolor)
   }
   return (
     <>
     
       <StatusBar style="auto" backgroundColor={randomBackground}/>
       <View style={[styles.container,{backgroundColor: randomBackground}]}>
+        <View style={[styles.shape,{backgroundColor: shapeColor}]}>
+          
+          
+        </View>
         <TouchableOpacity onPress={generateColor}>
           <View style={styles.actionBtn}>
             <Text style={styles.actionBtnText}>Press me</Text>
@@ -47,5 +55,12 @@ const styles = StyleSheet.create({
     textTransform: 'uppercase',
     color : '#fff'
 
+  },
+  shape:{
+    marginBottom: 50,
+    width: 100,
+    height: 100,
+    borderRadius: 50,
+    backgroundColor: 'black'
   }
 });
